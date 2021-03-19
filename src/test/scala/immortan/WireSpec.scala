fd8cf57a91e05e20b06e9d47bb60ab92fd59dcec
anton <akumaigorodski@gmail.com>
2021-03-19 17:44:44 +0200

    Add more wire tests

--- src/test/scala/immortan/WireSpec.scala
+++ src/test/scala/immortan/WireSpec.scala
@@ -2,9 +2,14 @@ package immortan
 
 import fr.acinq.eclair._
 import fr.acinq.eclair.wire._
+import fr.acinq.eclair.crypto.SphinxSpec._
 import fr.acinq.eclair.wire.LightningMessageCodecs._
+import fr.acinq.bitcoin.{ByteVector32, SatoshiLong}
+
+import fr.acinq.eclair.wire.Onion.FinalLegacyPayload
+import fr.acinq.eclair.crypto.Sphinx.PaymentPacket
+import fr.acinq.eclair.channel.CMD_ADD_HTLC
 import org.scalatest.funsuite.AnyFunSuite
-import fr.acinq.bitcoin.SatoshiLong
 
 
 class WireSpec extends AnyFunSuite {
@@ -33,7 +38,7 @@ class WireSpec extends AnyFunSuite {
     assert(decoded == msg2)
   }
 
-  test("Normal does not wrap normal messages") {
+  test("NC does not wrap normal messages") {
     val packet = OnionRoutingPacket(version = 2, publicKey = randomBytes(33), payload = randomBytes(1300), hmac = randomBytes32)
     val add = UpdateAddHtlc(randomBytes32, id = 100L, amountMsat = 1000000L.msat, paymentHash = randomBytes32, cltvExpiry = CltvExpiry(288), onionRoutingPacket = packet)
 
@@ -52,5 +57,19 @@ class WireSpec extends AnyFunSuite {
     val encoded = lightningMessageCodecWithFallback.encode(msg1).require
     val decoded = lightningMessageCodecWithFallback.decode(encoded).require.value
     assert(decoded == msg1)
+  }
+
+  test("UpdateAddHtlc tag encryption and partId equivalence") {
+    LNParams.format = MnemonicExtStorageFormat(outstandingProviders = Set.empty, LightningNodeKeys.makeFromSeed(randomBytes(32).toArray), seed = None)
+
+    val payload = FinalLegacyPayload(MilliSatoshi(10000L), CltvExpiry(144))
+    val packetAndSecrets = PaymentPacket.create(sessionKey, publicKeys, variableSizePayloadsFull, associatedData)
+    val fullTag = FullPaymentTag(paymentHash = ByteVector32.Zeroes, paymentSecret = ByteVector32.One, tag = PaymentTagTlv.LOCALLY_SENT)
+    val cmd = CMD_ADD_HTLC(fullTag, firstAmount = 1000000L.msat, CltvExpiry(144), packetAndSecrets, payload)
+
+    val encryptedTag: TlvStream[Tlv] = TlvStream(PaymentTagTlv.EncryptedPaymentSecret(cmd.encryptedTag) :: Nil)
+    val add = UpdateAddHtlc(randomBytes32, id = 1000L, cmd.firstAmount, cmd.fullTag.paymentHash, cmd.cltvExpiry, cmd.packetAndSecrets.packet, encryptedTag)
+    assert(cmd.partId == add.partId)
+    assert(add.fullTag == fullTag)
   }
 }
