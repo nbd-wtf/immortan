18ab5b3a042b8851df3ce285c356147f1ddfc264
fiatjaf <fiatjaf@gmail.com>
2022-07-01 14:28:42 -0300

    make notifyAppAvailable a noop member of ConnectionProvider trait.
    SBW needs this for some reason.

--- src/main/scala/immortan/ConnectionProvider.scala
+++ src/main/scala/immortan/ConnectionProvider.scala
@@ -1,10 +1,12 @@
 package immortan
 
 import java.net.{InetSocketAddress, Socket}
+import immortan.crypto.Tools.none
 
 trait ConnectionProvider {
   val proxyAddress: Option[InetSocketAddress]
   def getSocket: Socket
   def doWhenReady(action: => Unit): Unit
   def get(url: String): String
+  def notifyAppAvailable: Unit = none
 }
