7e23b158e90ebf16b51c0c0fe9009e0c4f60919e
fiatjaf <fiatjaf@gmail.com>
2022-07-27 14:13:34 -0300

    adjust one connection provider method.

--- src/main/scala/immortan/ConnectionProvider.scala
+++ src/main/scala/immortan/ConnectionProvider.scala
@@ -1,12 +1,11 @@
 package immortan
 
 import java.net.{InetSocketAddress, Socket}
-import immortan.crypto.Tools.none
 
 trait ConnectionProvider {
   val proxyAddress: Option[InetSocketAddress]
   def getSocket: Socket
   def doWhenReady(action: => Unit): Unit
   def get(url: String): String
-  def notifyAppAvailable: Unit = none
+  def notifyAppAvailable(): Unit = {}
 }
